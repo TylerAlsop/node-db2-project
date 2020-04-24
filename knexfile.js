07a550033cb2989e81bfdaac55dba45411ba1331
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-24 17:54:19 -0600

    Changed the filename route in knexfile.js to match the MVP.

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,6 @@ module.exports = {
   client: "sqlite3", // Specifying the DBMS
   useNullAsDefault: true, //  A flag that's required for SQLite specifically
   connection: {
-    filename: "./data/cars.db3" // Location of our database file
+    filename: "./data/car-dealer.db3" // Location of our database file
   }
 }
