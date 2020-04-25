7bad599ea52c2ff8b111e3dbd3eaa56e3dd4b283
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-24 18:32:31 -0600

    Rolled back the migration to change the year column in cars to be an integer and not a text datatype.

--- migrations/20200424180747_car-dealer.js
+++ migrations/20200424180747_car-dealer.js
@@ -2,7 +2,7 @@ exports.up = async function(knex) {
     await knex.schema.createTable("cars", (table) => {
         table.increments("id")
         table.text("vin").notNull().unique()
-        table.text("year").notNull()
+        table.integer("year").notNull()
         table.text("make").notNull()
         table.text("model").notNull()
         table.integer("mileage").notNull()
