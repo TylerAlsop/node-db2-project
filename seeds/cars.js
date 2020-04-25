f74329effc9ef79fbafafab44e74ba587187deae
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-24 18:51:11 -0600

    MVP and 1st strech goal reached. Seed data is setup for the cars table.

--- seeds/cars.js
+++ seeds/cars.js
@@ -1,13 +1,11 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function(knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert([
+    {vin: 'KSA7FJ2KMLA9AJ', year: 2010, make: 'Honda', model: 'CR-V', mileage: 119506, transmission: 'Automatic', title: 'clean'},
+    {vin: 'ALKDA8AVAV52N2', year: 2012, make: 'Chevrolet', model: 'Sonic', mileage: 56708, transmission: 'Automatic', title: 'clean'},
+    {vin: 'QNNRL8GS3M20B3', year: 2011, make: 'Toyota', model: 'Corolla', mileage: 70938, transmission: 'Automatic', title: 'salvage'},
+    {vin: 'Q9QRKN2K3RN202', year: 2010, make: 'Tesla', model: '3', mileage: 30456, transmission: 'Electric', title: 'clean'},
+  ]);
+
 };
